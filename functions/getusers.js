0eeb5e3d77f8bf767452f70f69d2e2c603f66f38
Sai Prasad <sai.prasad@rocketmail.com>
2019-01-01 22:29:59 +0530

    UI page, javascript to fetch users from local lambda

--- functions/getusers.js
+++ functions/getusers.js
@@ -2,12 +2,32 @@ const axios = require('axios')
 
 
 exports.handler = (event, context, callback) => {
-    const {name, likes} = JSON.parse(event.body);
 
-    const mesg = JSON.stringify({ msg: 'Hello ' + name + ' likes ' + likes })
+    const API_URL= 'https://api.github.com/users';
+    const API_CLIENT_ID=  '62d9feb46632996f62e3';
+    const API_CLIENT_SECRET = 'c46f0312fc3467b96ace4d7abadefdf6cd94a175';
 
-    callback(null, {
-        statusCode: 200,
-        body: mesg
-    })
+    const URL = `${API_URL}?client_id=${API_CLIENT_ID}&client_secret=${API_CLIENT_SECRET}`
+
+    // REsponse handler
+    const send = body => {
+        callback(null, {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin' : '*',
+                'Access-Control-Allow-Headers' : 
+                'Origin, X-Requested-With, Content-Type, Accept'
+            },
+            body: JSON.stringify(body)
+        })
+    }
+
+    const getUsers = () => {
+        axios.get(URL)
+            .then( res => send(res.data))
+            .catch(err => send(err))
+    }
+
+    if(event.httpMethod == 'GET') getUsers();
+
 }
---
+++ js/main.js
@@ -0,0 +1,36 @@
+const fetchUsers = async() =>
+    await (await fetch('http://localhost:9000/getusers')).json();
+
+    // .netlify/functions/getusers - PROD
+    // http://localhost:9000/getusers
+
+
+fetchUsers().then(data => {
+
+    userList = document.querySelector('#users');
+    const MAX_LIST = 5;
+
+    data.forEach(user => {
+
+     
+            const li = document.createElement('li');
+            li.className = 'list-group-item';
+
+            const image = document.createElement('img');
+            image.src = user.avatar_url + "&s=120";
+            image.style.borderRadius='50%';
+            image.style.width='40px';
+            image.style.height='40px';
+            const link = document.createElement('a');
+
+            link.appendChild(document.createTextNode(user.login));
+            link.href = user.html_url;
+            link.className = "ml-3"
+            link.target = '_blank';
+            li.appendChild(image);
+            li.appendChild(link);
+            userList.appendChild(li);
+                  
+    });
+
+})
