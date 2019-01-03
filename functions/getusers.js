e4687f3f4b263fe055be6403622deae10f2c1375
Sai Prasad <sai.prasad@rocketmail.com>
2019-01-03 14:36:09 +0530

    Adding count bubble

--- functions/getusers.js
+++ functions/getusers.js
@@ -3,7 +3,11 @@ const axios = require('axios')
 
 exports.handler = (event, context, callback) => {
 
-    const {API_URL, API_CLIENT_ID, API_CLIENT_SECRET} = process.env
+    const {
+        API_URL,
+        API_CLIENT_ID,
+        API_CLIENT_SECRET
+    } = process.env
 
     let URL = `${API_URL}?client_id=${API_CLIENT_ID}&client_secret=${API_CLIENT_SECRET}`
     URL = API_URL
@@ -13,9 +17,8 @@ exports.handler = (event, context, callback) => {
         callback(null, {
             statusCode: 200,
             headers: {
-                'Access-Control-Allow-Origin' : '*',
-                'Access-Control-Allow-Headers' : 
-                'Origin, X-Requested-With, Content-Type, Accept'
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
             },
             body: JSON.stringify(body)
         })
@@ -23,10 +26,10 @@ exports.handler = (event, context, callback) => {
 
     const getUsers = () => {
         axios.get(URL)
-            .then( res => send(res.data))
+            .then(res => send(res.data))
             .catch(err => send(err))
     }
 
-    if(event.httpMethod == 'GET') getUsers();
+    if (event.httpMethod == 'GET') getUsers();
 
 }
--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const fetchUsers = async() =>
     await (await fetch('/.netlify/functions/getusers')).json();
 
-    // /.netlify/functions/getusers - PROD
-    // http://localhost:9000/getusers
+// /.netlify/functions/getusers - PROD
+// http://localhost:9000/getusers
 
 
 fetchUsers().then(data => {
@@ -11,31 +11,31 @@ fetchUsers().then(data => {
     const MAX_LIST = 5;
 
     data.forEach(user => {
-            const header = document.querySelector('h1.text-center.display-4')
-            header.innerHTML = header.textContent.split(/\(/)[0] + "("+ data.length +")"
-        
-            const li = document.createElement('li');
-            li.className = 'list-unstyled';
-            const div = document.createElement('div');
-            div.className = "card card-body mt-1 d-inline-block";
+        const header = document.querySelector('h1.text-center.display-4')
+        header.setAttribute("data-count", " " + data.length + " ");
 
-            const image = document.createElement('img');
-            image.src = user.avatar_url + "&s=90";
-            image.style.borderRadius='50%';
-            image.style.width='40px';
-            image.style.height='40px';
-            const link = document.createElement('a');
+        const li = document.createElement('li');
+        li.className = 'list-unstyled';
+        const div = document.createElement('div');
+        div.className = "card card-body mt-1 d-inline-block";
 
-            link.appendChild(document.createTextNode(user.login));
-            link.href = user.html_url;
-            link.className = "ml-3"
-            link.target = '_blank';
-            
-            div.appendChild(image);
-            div.appendChild(link);
-            li.appendChild(div);
-            userList.appendChild(li);
-                  
+        const image = document.createElement('img');
+        image.src = user.avatar_url + "&s=90";
+        image.style.borderRadius = '50%';
+        image.style.width = '40px';
+        image.style.height = '40px';
+        const link = document.createElement('a');
+
+        link.appendChild(document.createTextNode(user.login));
+        link.href = user.html_url;
+        link.className = "ml-3"
+        link.target = '_blank';
+
+        div.appendChild(image);
+        div.appendChild(link);
+        li.appendChild(div);
+        userList.appendChild(li);
+
     });
 
 })
